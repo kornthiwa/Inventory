af05fa820a8fc269e3a005ebe2a94305b2c37bdd
Ford-Kornthiwa <kornthiwa@gmail.com>
2025-11-18 10:01:12 +0700

    update readme

--- web/app/lib/api.ts
+++ web/app/lib/api.ts
@@ -4,7 +4,7 @@ import axios, {
   InternalAxiosRequestConfig,
 } from "axios";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
